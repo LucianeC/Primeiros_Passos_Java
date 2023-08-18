27688a3c2a8c76fa7b51a5e15a7ca71e3a2e3a1b
LucianeC <costaluciane087@gmail.com>
2023-08-18 15:23:53 -0300

    Initial commit

---
+++ src/Anotacao.java
@@ -0,0 +1,24 @@
+public class Anotacao {
+    //Operadores de atribução
+    int valor1 =  5;  //Atribui o valor 5 à variável valor
+
+    //Operadores aritméticos
+//            "+" (adição)
+//            "-" (subtração)
+//            "*" (multiplicação)
+//            "/" (divisão)
+//            "%" (resto da divisão)
+    int a = 10 + 5; // Atribui o valor 15 à variável a
+    int b = 10 - 5; // Atribui o valor 5 à variável b
+    int c = 10 * 5; // Atribui o valor 50 à variável c
+    int d = 10 / 5; // Atribui o valor 2 à variável d
+    int e = 10 % 3; // Atribui o valor 1 à variável e (o resto da divisão de 10 por 3 é 1)
+
+    //Operadores relacionais
+//            "==" (igual a)
+//            "!=" (diferente de)
+//            ">" (maior que)
+//            ">=" (maior ou igual a)
+//            "<" (menor que)
+//            "<=" (menor ou igual a)
+}
---
+++ src/Teste.java
@@ -0,0 +1,18 @@
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("bem vindo ao mundo Screen Match");
+        System.out.println("Filme: Ta chovendo Hamburguer");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFileme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = "Um desenho de aventura e ação, que tem como climax chuvas de comida";
+        System.out.println(sinopse);
+
+    }
+}
